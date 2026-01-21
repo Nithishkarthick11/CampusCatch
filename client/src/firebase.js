d2096ab0da22c2e8e78c9989b72dfd976e2e7ca7
Nithishkarthick11 <nithishkarthick.s.2023.cse@ritchennai.edu.in>
2026-01-21 22:28:25 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -14,6 +14,7 @@ const firebaseConfig = {
   messagingSenderId: "142210430980",
   appId: "1:142210430980:web:630ea3a4fe2b9676ba1d51",
   measurementId: "G-Y6NX4B89WZ"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
